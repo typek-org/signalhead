07abb3fe74c3df68d481cce96457368f77a61f10
m93a <m93a.cz@gmail.com>
2024-03-18 17:17:02 +0100

    fix: export filter, skip & withHistory

--- src/lib/signals/mod.ts
+++ src/lib/signals/mod.ts
@@ -5,10 +5,14 @@ export * from "./writable.ts";
 
 export * from "./cons.ts";
 export * from "./count.ts";
-export * from "./tap.ts";
 export * from "./enumerate.ts";
+export * from "./filter.ts";
 export * from "./flat.ts";
 export * from "./flatMap.ts";
 export * from "./map.ts";
 export * from "./scan.ts";
+export * from "./skip.ts";
+export * from "./skipEqual.ts";
+export * from "./tap.ts";
+export * from "./withHistory.ts";
 export * from "./zip.ts";
