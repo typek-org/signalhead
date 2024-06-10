d99096a0502aeb41197f387268d6ffdcfd156423
Ondra Janoška <o.janoska98@gmail.com>
2024-06-10 12:31:06 +0200

    fix(mutDerived): set now correctly updates prev

--- src/lib/functions/mutDerived.ts
+++ src/lib/functions/mutDerived.ts
@@ -5,6 +5,7 @@ import {
 	type MinimalSignal,
 	type WritableSignal,
 } from "../signals/mod.ts";
+import { setterSideEffectSignal } from "../signals/setterSideEffect.ts";
 
 /**
  * Sometimes you need a writable signal whose value automatically
@@ -58,5 +59,8 @@ export function mutDerived<T>(
 		},
 	});
 
-	return signal;
+	return setterSideEffectSignal(signal, (newValue: T) => {
+		value = newValue;
+		return newValue;
+	});
 }
